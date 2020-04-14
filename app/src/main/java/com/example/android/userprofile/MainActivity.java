2a8c23b0f0e7be4ac008c132de5446bfe19884c5
JennaKoslowski <jenna.koslowski@yahoo.com>
2020-04-13 20:37:58 -0400

    get text onCreate

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.userprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textViewName = (TextView) findViewById(R.id.name);
+        textViewName.setText("Jenna");
+
+        TextView textViewBirthday = (TextView) findViewById(R.id.birthday);
+        textViewBirthday.setText("April 13, 2020");
+
+        TextView textViewCountry = (TextView) findViewById(R.id.country);
+        textViewCountry.setText("USA");
+
     }
 }
